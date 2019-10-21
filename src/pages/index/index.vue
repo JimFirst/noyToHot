8477aed7880facb8f637c5285fae5c8b1cc60e4f
dongmingjie <154211320@qq.com>
2019-10-21 09:46:50 +0800

    初始化

---
+++ src/pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    我是首页
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      
+    }
+  },
+
+  
+
+  methods: {
+    
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style>
+</style>
