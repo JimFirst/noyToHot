af0be6406ab97a675086a2bf50cfdca81756dbdd
dongmingjie <154211320@qq.com>
2019-10-22 13:50:50 +0800

    云开发

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App'
-
+import './database/index'
+// 封装云开发数据库
+import db from './database'
+Vue.prototype.$db = db
 Vue.config.productionTip = false
 App.mpType = 'app'
-
 const app = new Vue(App)
 app.$mount()
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     我是首页
+    <img src="cloud://dev-e18550.6465-dev-e18550/images/advantagefour.png" alt="" srcset="">
+    <button @click="test">测试</button>
   </div>
 </template>
 
@@ -12,15 +14,15 @@ export default {
       
     }
   },
-
-  
-
   methods: {
-    
+    test() {
+      this.$db.get('message').then(res => {
+        console.log(res)
+      })
+    }
   },
-
   created () {
-    // let app = getApp()
+
   }
 }
 </script>
